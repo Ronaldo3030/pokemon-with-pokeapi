eaf19bfde8d50117a4f5c997648e20735681aeba
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-12-03 16:37:34 -0300

    ajustado excluir todos os favoritos, e algumas cores dos favoritos

---
+++ assets/js/functions/showPokemons.js
@@ -0,0 +1,19 @@
+async function showPokemons() {
+  let pokemons = await getAllPokemons("https://pokeapi.co/api/v2/pokemon/?limit=24");
+  loading.classList.add("d-none");
+  containerPokemons.innerHTML = "";
+  containerPokemons.classList.remove("d-none");
+
+  let favoritesPokemonLocalStorage = getAllStorage();
+
+  for (let pokemon of await pokemons) {
+    let nameOfPokemon = pokemon.name;
+    let imgOfPokemon = pokemon.sprites.other["official-artwork"].front_default;
+    let orderOfPokemon = pokemon.order;
+    let typeOfPokemon = [];
+    for (let type of pokemon.types) {
+      typeOfPokemon.push(type.type.name.capitalize());
+    }
+    createCard("container-pokemons", imgOfPokemon, nameOfPokemon.capitalize(), orderOfPokemon, typeOfPokemon, favoritesPokemonLocalStorage);
+  }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,8 @@ const container = $("#container");
 const containerPokemons = document.getElementById("container-pokemons");
 const loading = document.getElementById("loading-pokemon");
 
-$(document).ready(async () => {
-  let pokemons = await getAllPokemons("https://pokeapi.co/api/v2/pokemon/?limit=24");
-  loading.classList.add("d-none");
-  containerPokemons.classList.remove("d-none");
+$(document).ready(async () => showPokemons());
 
-  let favoritesPokemonLocalStorage = getAllStorage();
-
-  for(let pokemon of await pokemons){
-    let nameOfPokemon = pokemon.name;
-    let imgOfPokemon = pokemon.sprites.other["official-artwork"].front_default;
-    let orderOfPokemon = pokemon.order;
-    let typeOfPokemon = [];
-    for(let type of pokemon.types){
-      typeOfPokemon.push(type.type.name.capitalize());
-    }
-    createCard("container-pokemons", imgOfPokemon, nameOfPokemon.capitalize(), orderOfPokemon, typeOfPokemon, favoritesPokemonLocalStorage);
-  }
-});
-
-String.prototype.capitalize = function() {
-	return this.charAt(0).toUpperCase() + this.substr(1);
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.substr(1);
 }
