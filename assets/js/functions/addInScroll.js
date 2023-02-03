ef2ccf8776a849714b19fa17b7a529c0d46ea3ba
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2023-02-03 14:09:27 -0300

    adjusted in spinner load was appearing every time

--- assets/js/functions/addInScroll.js
+++ assets/js/functions/addInScroll.js
@@ -2,8 +2,8 @@ let position = 24;
 let requestMade = false;
 window.addEventListener('scroll', async () => {
   let quantityPokemons = 24;
-  $("#loader").css("display", "flex");
   if ($(window).scrollTop() + $(window).height() >= $(document).height() && !requestMade) {
+    $("#loader").css("display", "flex");
     requestMade = true;
     await showPokemons(position, quantityPokemons);
     position = position + quantityPokemons;
